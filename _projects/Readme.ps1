8783d7f8ead41c952552b1ee585bd253f2dff9a6
mmuras@gmail.com <matthew.muras@everi.com>
2019-07-30 04:21:40 -0500

    Adding Step numbers

--- _projects/Readme.ps1
+++ _projects/Readme.ps1
@@ -8,16 +8,17 @@
     I usually access github from within Visual Studio Code.  As such, when I start coding a new project, I often need a reminder, or a cheat sheet for how to connect Visual Studio Code to a Git repository.  These notes are more for me than for anyone else, but I'm sharing them nonetheless.
 
         Steps:
-        Create a directory on the local file system.
-        Create a repo on Github.
-        Select Clone "Clone or download" on Github, copy the link
-        In Visual Studio Code, sect File -> Add Folder to Workspace -> Select the newly created directory
-        Select Terminal Window
-        In the window, type:
-        git config --global user.name <github userID>
+            1. Create a directory on the local file system.
+            2. Create a repo on Github.
+            3. Select Clone "Clone or download" on Github, copy the link
+            4. In Visual Studio Code, sect File -> Add Folder to Workspace -> Select the newly created directory
+            5. Select Terminal Window
+            6. In the window, type:
+                git config --global user.name <github userID>
 
-        git clone <URL from github link copied earlier>
-        That should be all that's required.  any newly created file should be available on github after stage/commit/push.
+                git clone <URL from github link copied earlier>
+
+                That should be all that's required.  any newly created file should be available on github after stage/commit/push.
 
 #
 #>
